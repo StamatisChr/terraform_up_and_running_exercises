6f5aebe7cb7de436b37122dc0ef4ecd94da401c4
StamatisChr <stamatios.chrysinas@hashicorp.com>
2024-12-04 10:28:42 +0100

    changed db instance class, t2 is not supported

--- 06-managing-secrets-with-terraform/mysql-kms/main.tf
+++ 06-managing-secrets-with-terraform/mysql-kms/main.tf
@@ -28,7 +28,7 @@ resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
   engine              = "mysql"
   allocated_storage   = 10
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.small"
   skip_final_snapshot = true
   db_name             = var.db_name
 
